b603c8488e2b42c47d15a264b236dba4f7fe708d
Douglas <douglasclaudino10.dc@gmail.com>
2023-09-20 01:33:01 -0300

    fix webdriver

--- aramdetails.py
+++ aramdetails.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchDriverException
 from selenium.webdriver.common.by import By
 import os
 from dotenv import load_dotenv
@@ -9,8 +10,9 @@ class AramDetails:
     def __init__(self):
         self.keys = ["name", "winrate", "matches"]
 
+
     def get_top_champions(self, total: int):
-        driver = webdriver.Chrome()
+        driver = self.get_default_browser()
         driver.get(os.getenv("ARAM_WEB_URL"))
         rows = driver.find_elements(By.CLASS_NAME, "rt-tr")
         rows.pop(0)
@@ -26,3 +28,20 @@ class AramDetails:
         driver.close()
         response_dict: [dict] = [{k: v for (k, v) in zip(self.keys, infos)} for infos in items]
         return response_dict
+    
+    def get_default_browser(self):
+        try:
+            return webdriver.Firefox()
+        except NoSuchDriverException as ex:
+            print('firefox')
+            print(ex)
+        try:
+            return webdriver.Chrome()
+        except NoSuchDriverException as ex:
+            print('chrome')
+            print(ex)
+        try:
+            return webdriver.Ie()
+        except NoSuchDriverException as ex:
+            print('IE')
+            print(ex)
